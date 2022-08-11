ae7e1aa396ee15e7701709b5e779729722d52c70
Joel-Irineu <irineu.joel01@gmail.com>
2022-08-11 16:02:40 -0300

    versão com materialize

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estacionamento</title>
+    
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+           
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+</head>
+<body>
+    <div class="container">
 <?php
     $pdo = new PDO('mysql:host=localhost;dbname=parking', 'root', '');
 
@@ -15,12 +34,37 @@
     
 ?>
 
-<form action="" method="post"> 
+<div class="row">
+    <h3>Sistema de estacionamento </h3>
+    <form class="col s12" method="post">
+      <div class="row">
+        <div class="input-field col s4">
+          <i class="material-icons prefix">account_circle</i>
+          <input id="client" type="text" class="validate" name="client">
+          <label for="client">Nome do Cliente</label>
+        </div>
+        <div class="input-field col s4">
+          <i class="material-icons prefix">directions_car</i>
+          <input id="modelCar" type="tel" class="validate" name="modelCar">
+          <label for="modelCar">Modelo do carro</label>
+        </div>
+        <div class="input-field col s4">
+          <i class="material-icons prefix">fiber_pin</i>
+          <input id="licensePlate" type="tel" class="validate" name="licensePlate">
+          <label for="licensePlate">Placa</label>
+        </div>
+      </div>
+      <input class="waves-effect waves-light btn text-white" style="color: #fff !important" type="submit" value="enviar">
+    </form>
+  </div>
+        
+
+<!-- <form action="" method="post"> 
     <input type="text" placeholder="Nome do cliente" name="client" id="client">
     <input type="text" placeholder="Modelo do carro" name="modelCar" id="modelCar">
     <input type="text" placeholder="Placa do carro" name="licensePlate" id="licensePlate">
     <input type="submit" value="enviar">
-</form>
+</form> -->
 
 <?php
     $sql = $pdo->prepare("SELECT * FROM parked ORDER BY id DESC");
@@ -55,19 +99,22 @@
             print   '<td>'.$parked['timeEnd'].'</td>';
             print   '<td> R$'.$parked['price'].'</td>';
         }
-        print   '<td><a href="edit/?id='.$parked['id'].'&client='.$parked['client'].'&modelCar='.$parked['modelCar'].'&licensePlate='.$parked['licensePlate'].'"> editar </a></td>';
+        print   '<td><a class="waves-effect waves-light btn yellow lighten-1 text-black" title="Editar" href="edit/?id='.$parked['id'].'&client='.$parked['client'].'&modelCar='.$parked['modelCar'].'&licensePlate='.$parked['licensePlate'].'"><i class="material-icons right">create</i> </a></td>';
         
         if($parked['timeEnd'] == ''){
-            print   '<td><a href="end/?id='.$parked['id'].'"> Finalizar </a></td>';
+            print   '<td><a class="waves-effect waves-light btn" title="Finalizar" href="end/?id='.$parked['id'].'"> <i class="material-icons right">done_all</i> </a></td>';
         }else{
-            print   '<td style="color: #787878">Finalizar</td>';
+            print   '<td><i class="material-icons right waves-effect waves-light btn grey lighten-1" title="Finalizar">done_all</i></td>';
         }
         if($parked['timeEnd'] == ''){
-            print   '<td style="color: #787878">Comprovante</td>';
+            print   '<td><i class="material-icons right waves-effect waves-light btn grey lighten-1" title="Imprimir comprovante">local_printshop</i></td>';
         }else{
-            print   '<td><a href="print/?id='.$parked['id'].'" target="_blank"> Comprovante </a></td>';
+            print   '<td><a class="waves-effect waves-light btn" href="print/?id='.$parked['id'].'" target="_blank" title="Imprimir comprovante"><i class="material-icons right">local_printshop</i></a></td>';
         }
         print'<tr>';
     }
 ?>
     </table>
+    </div>
+</body>
+</html>
